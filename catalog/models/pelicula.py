edb951a9523fac38be7af2b05ff8d0e038c5be45
Sebas <sebastian.rubio@uisek.edu.ec>
2026-07-15 19:40:39 -0500

    Gitignore creado para que el .env ya no se suba

--- catalog/models/pelicula.py
+++ catalog/models/pelicula.py
@@ -9,6 +9,22 @@ class Pelicula(models.Model):
     director = models.ForeignKey(Director, on_delete=models.PROTECT, related_name='peliculas')
     vendedores = models.ManyToManyField(Vendedor, related_name='peliculas', blank=True)
     poster = models.ImageField(upload_to='posters/', null=True, blank=True)
+    genero_choices = [
+        ('ACCION', 'Acción'),
+        ('COMEDIA', 'Comedia'),
+        ('DRAMA', 'Drama'),
+        ('TERROR', 'Terror'),
+        ('CIENCIA_FICCION', 'Ciencia Ficción'),
+        ('ANIMACION', 'Animación'),
+        ('DOCUMENTAL', 'Documental'),
+        ('ROMANCE', 'Romance'),
+        ('AVENTURA', 'Aventura'),
+        ('FANTASIA', 'Fantasía'),
+        ('MUSICAL', 'Musical'),
+        ('SUSPENSE', 'Suspense'),
+        ('WESTERN', 'Western'),
+    ]
+    genero = models.CharField(max_length=50, choices=genero_choices, default='DRAMA')
 
     def __str__(self):
         return self.nombre
